47535a71bc7e71882a0b2f79ab95fca550f6ab95
Manav Punjabi <manavpunjabi26@gmail.com>
2020-03-26 22:03:58 +0530

    added dashboard along with create profile form

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,11 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import PrivateRoute from "./components/routing/PrivateRoute.js";
 import "./App.css";
+
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -31,6 +35,12 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
