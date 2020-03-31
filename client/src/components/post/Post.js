a64b083369e3fafe68974ba853e60447fd753ad9
Manav Punjabi <manavpunjabi26@gmail.com>
2020-03-31 18:33:22 +0530

    completed posts, post and comment components

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,43 @@
+import React, { useEffect, Fragment } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getPost, getPosts } from "../../actions/post";
+import PostItem from "../posts/PostItem";
+import Spinner from "../layout/Spinner";
+import CommentForm from "./CommentForm";
+import CommentItem from "./CommentItem";
+import { Link } from "react-router-dom";
+
+const Post = ({ getPost, post: { post, loading }, match }) => {
+  useEffect(() => {
+    getPost(match.params.id);
+  }, getPosts);
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to="/posts" className="btn">
+        Back to all posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
+    </Fragment>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(mapStateToProps, { getPost })(Post);
